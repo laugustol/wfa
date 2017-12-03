0b37f17afb5b2ac3758d2f07efda43181d7a1d86
Augusto Alvarez <augustoalvarez05@gmail.com>
2017-12-03 14:16:57 -0400

    Initiated commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var five = require ('johnny-five');
+var app = express();
+var socketIO = require('socket.io')(app.listen(8080));
+
+app.use(express.static('public'));
+app.set("view engine","jade");
+
+app.get('/', function (req,res) { 
+  	res.render("index");
+});
+
+var board = new five.Board({
+  	repl:false
+});
+ 
+board.on("ready", function() {
+  var leds = new five.Leds([9, 10, 11]);
+  socketIO.on('connection',function(io){
+  	console.log("Connection with the client");
+  	io.on("ledOne",function(){
+  		leds[0].blink(300);
+  		console.log("ledOne");
+  	});
+  	io.on("ledTwo",function(){
+  		leds[1].blink(300);
+  		console.log("ledTwo");
+  	});
+  	io.on("ledThree",function(){
+  		leds[2].blink(300);
+  		console.log("ledThree");
+  	});
+  });
+});
+
+console.log("Server ON");
